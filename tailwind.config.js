4b098e590869ea23d25e6dd8375a4ff7726f7d60
Charlotte Moche <cmoche123@gmail.com>
2024-07-24 19:28:41 -0400

    move

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.js'],
+  content: ['./**/*.html', './src/**/*.js'],
   theme: {
     extend: {
       colors: {
